5dd0a68a925f75299d51b5c8386f80ff8305a57a
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2023-08-12 22:35:41 +0200

    sha256 fix

--- Formula/hello-brew.rb
+++ Formula/hello-brew.rb
@@ -3,8 +3,8 @@ class HelloBrew < Formula
 
   desc "Example Python console app to play with Homebrew"
   homepage "https://github.com/jackinf/HellowBrew"
-  url "https://github.com/jackinf/HelloBrew/releases/download/1.2/hello-brew.tar.gz"
-  sha256 "20f4bab5e35888b16e9b3b400e9e14cca8890c0a2577e1aa2e8dc00316048b29"
+  url "https://github.com/jackinf/HelloBrew/releases/download/1.1/hello-brew.tar.gz"
+  sha256 "c7c91a7e7ee09e7ea9230fe69694050ffd200096b85172fd1cc79e32da9bc203"
   license "MIT"
 
   # depends_on "python@3.11"
