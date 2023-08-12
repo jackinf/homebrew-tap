de8129e34b524b6875ab06d11318db7aec1e2b9f
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2023-08-12 22:36:13 +0200

    123

--- Formula/hello-brew.rb
+++ Formula/hello-brew.rb
@@ -4,7 +4,7 @@ class HelloBrew < Formula
   desc "Example Python console app to play with Homebrew"
   homepage "https://github.com/jackinf/HellowBrew"
   url "https://github.com/jackinf/HelloBrew/releases/download/1.1/hello-brew.tar.gz"
-  sha256 "c7c91a7e7ee09e7ea9230fe69694050ffd200096b85172fd1cc79e32da9bc203"
+  sha256 "358f9e8659569a4b86d1d47baa8dd0c75de1d0f40722c06c60c188ef05b0fb95"
   license "MIT"
 
   # depends_on "python@3.11"
