80f515c266f2a08ff0246954baa4b8c9d0928a63
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2023-08-12 21:42:50 +0200

    updated version

--- hello-brew/hello-brew.rb
+++ hello-brew/hello-brew.rb
@@ -3,14 +3,15 @@ class HelloBrew < Formula
 
   desc "Example Python console app to play with Homebrew"
   homepage "https://github.com/jackinf/HellowBrew"
-  url "https://github.com/jackinf/homebrew-tap/releases/download/0.1/hello-brew.tar.gz"
-  sha256 "278191488D87CE96F01300B08CA81163384A11101DEBAFEF9807FCE890BF6A64"
+  url "https://github.com/jackinf/homebrew-tap/releases/download/0.2/hello-brew.tar.gz"
+  sha256 "A06272CF04A6076B63584CE6A5746E4BF7733177A82220F45D921D9EE9EBACE0"
   license "MIT"
 
   # depends_on "python@3.11"
 
   def install
     bin.install "hello-brew.py" => "hello-brew"
+    chmod 0755, bin/"hello-brew"
   end
 
   test do
